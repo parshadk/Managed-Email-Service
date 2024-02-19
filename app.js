3add7ce8d56777f7103867eeb68c4cc14f2254c5
Parshad <138670180+parshadk@users.noreply.github.com>
2024-02-19 14:24:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from "express";
-import routes from "./routes/index.js"
+import routes from "./routes/index.js";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __dirname=dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(express.json());
+app.use(express.static(__dirname +'/public'));
 
 app.use('/', routes);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+ 'public/index.html');
+});
 
 const port = 3000;
 
